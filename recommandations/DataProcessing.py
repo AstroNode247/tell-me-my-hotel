a0d78b2f5ec54bd1fa351e0ffec09199ca137ee8
andry.anthony <astro@node.com>
2021-11-07 13:48:18 +0100

    Traiter les json imbriqués du dataframe

--- recommandations/DataProcessing.py
+++ recommandations/DataProcessing.py
@@ -5,30 +5,29 @@ import pandas as pd
 from tqdm import tqdm
 
 
-def to_json(data, prc):
-    data_list = []
-    for i in tqdm(range(0, int(len(data) * prc))):
-        data_list.append(json.loads(data[i]))
-
-    return data_list
-
-
 class RecsData:
     data = []
-
+    
     def __init__(self, path):
         self.data = []
         print('Initialize the dataset')
         with open(path) as f:
             for line in tqdm(f):
                 self.data.append(line.strip())
+    
+    def to_json(self, prc=1):
+        json_data = []
+        for i in tqdm(range(0, int(len(self.data) * prc))):
+            json_data.append(json.loads(self.data[i]))
+
+        return json_data
 
     def to_dataframe(self, prc=1):
         print('Parse into json')
-        data_list = to_json(self.data, prc=prc)
+        json_data = self.to_json(prc=prc)
 
         keys = []
-        for key in data_list[0]:
+        for key in json_data[0]:
             keys.append(key)
 
         data_dict = {}
@@ -36,7 +35,7 @@ class RecsData:
             data_dict[key] = []
 
         print('Read to dataframe')
-        for data in tqdm(data_list):
+        for data in tqdm(json_data):
             for key in data_dict:
                 try:
                     if data[key] == "" or data[key] is None:
